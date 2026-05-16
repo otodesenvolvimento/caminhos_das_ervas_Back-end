0db2d448cb4eb270bcdc4bf77659ad4dcf46c074
Oto marques <otodesenvolvimento@gmail.com>
2026-05-16 17:55:11 -0300

    Change home endpoint response format

--- src/app.py
+++ src/app.py
@@ -86,7 +86,7 @@ def check_admin():
 # =========================================
 @app.get("/")
 def home():
-    return {"<h1>Status: Online</h1>"}    
+    return {"status": "online"}  
 
 @app.post('/products/upload-excel')
 def upload_excel():
